4dab930c243170e88f998f38672768722683f1d2
Kartik Sajwan <kartik.sajwan@gmail.com>
2021-07-24 12:25:24 +0530

    loading data from api on all courses page

---
+++ src/app/search-sort/search-sort.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-sort',
+  templateUrl: './search-sort.component.html',
+  styleUrls: ['./search-sort.component.scss']
+})
+export class SearchSortComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
